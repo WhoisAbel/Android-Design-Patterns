2cf841ad12f50d6794ca241599a952e2da2c5eb3
Abolfazl Kalamati <abolfazl.kalamati@yahoo.com>
2022-03-27 08:50:02 +0430

    Create Database (#5)

---
+++ app/src/main/java/com/example/androiddesignpatterns/ShoppingItem.kt
@@ -0,0 +1,16 @@
+package com.example.androiddesignpatterns
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    @ColumnInfo(name = "item_name")
+    val name: String,
+    @ColumnInfo(name = "item_amount")
+    val amount: Int
+) {
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null
+}
