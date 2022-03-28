b66a812d72cd058c1eff0ff47c1bacb37f9b6bcd
Abolfazl Kalamati <abolfazl.kalamati@yahoo.com>
2022-03-28 08:53:52 +0430

    Kodein dependency injection (#9)

--- app/src/main/java/com/example/androiddesignpatterns/ui/ShoppingActivity.kt
+++ app/src/main/java/com/example/androiddesignpatterns/ui/ShoppingActivity.kt
@@ -3,28 +3,27 @@ package com.example.androiddesignpatterns.ui
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androiddesignpatterns.R
-import com.example.androiddesignpatterns.data.db.ShoppingDatabase
 import com.example.androiddesignpatterns.data.db.entities.ShoppingItem
-import com.example.androiddesignpatterns.data.repositories.ShoppingRepository
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.instance
+import org.kodein.di.android.kodein
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: ShoppingViewModelFactory by instance()
+
     @SuppressLint("CutPasteId", "NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
-
         val viewModel = ViewModelProvider(this, factory)[ShoppingViewModel::class.java]
-
 
         val adapter = ShoppingListAdapter(listOf(), viewModel)
         findViewById<RecyclerView>(R.id.rvShoppingItems).layoutManager = LinearLayoutManager(this)
